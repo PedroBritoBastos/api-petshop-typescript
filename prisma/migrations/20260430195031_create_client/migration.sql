3c6f9e49bbfc59fdc275a27ffac4d3f82939ec5c
pedro <pedrobritobastos234@gmail.com>
2026-04-30 16:55:39 -0300

    chore: setup prisma com sqlite3

---
+++ prisma/migrations/20260430195031_create_client/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "cpf" TEXT,
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE INDEX "Client_userId_idx" ON "Client"("userId");
