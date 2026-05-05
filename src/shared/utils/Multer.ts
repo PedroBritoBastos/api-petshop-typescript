f0a7d864ada6f5a81302911640d2eabd41057115
pedro <pedrobritobastos234@gmail.com>
2026-05-05 15:53:46 -0300

    feat: cria multer

--- src/shared/utils/Multer.ts
+++ src/shared/utils/Multer.ts
@@ -1,4 +1,19 @@
+import multer from "multer";
+
 /**
  * this class deals with uploads
  */
-export class Multer {}
+export class Multer {
+  private static storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+      let name = new Date().getDate() + "-" + file.originalname;
+      cb(null, name);
+    },
+    destination: function (req, file, cb) {
+      let path = "../../data/photos/users";
+      cb(null, path);
+    },
+  });
+
+  public static upload = multer({ storage: Multer.storage });
+}
