62babfdcfc10f0e548cf8d279d740ca862d08b16
pedro <pedrobritobastos234@gmail.com>
2026-06-21 10:27:21 -0300

    fix: pasta do prisma inserida em src

--- src/modules/petshopService/repositories/IPetshopServiceRepository.ts
+++ src/modules/petshopService/repositories/IPetshopServiceRepository.ts
@@ -1,4 +1,4 @@
-import { PetshopService } from "../../../../generated/prisma/client";
+import { PetshopService } from "../../../generated/prisma/client";
 import { CreatePetshopServiceDTO } from "../dtos/CreatePetshopServiceDTO";
 import { UpdatePetshopServiceDTO } from "../dtos/UpdatePetshopServiceDTO";
 
